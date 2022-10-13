ff1a6d4a02165df007d7507da559689a64ffeb11
Dmitriy Born <1111born1111@gmail.com>
2022-10-13 15:47:15 +0300

    Ex_2

--- Program.cs
+++ Program.cs
@@ -2,7 +2,6 @@
 //Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
 //3, 5 -> 243 (3⁵)
 //2, 4 -> 16
-
 Console.WriteLine("----------Задание №1----------");
 Console.WriteLine("Введите число, которое возводить в степень: ");
 int A = int.Parse(Console.ReadLine()!);
@@ -16,4 +15,23 @@ int Construction(int number, int degree){
         result = result * number;
     }
     return result;
+}
+
+//Задача 2
+//ННапишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+Console.WriteLine("----------Задание №2----------");
+Console.WriteLine("Введите число: ");
+int C = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"Сумма цифр в числе {C} = {Sum(C)}");
+
+int Sum(int num){
+    int R = 0;   
+    while (num > 0){
+        R = R + num % 10;
+        num = num / 10;   
+    }   
+    return R;
 }
